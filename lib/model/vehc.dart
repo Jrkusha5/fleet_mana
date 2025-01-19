2832312d8f624d120b125907f97b2711f4f2cfac
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2025-01-19 08:22:16 -0800

    first commit

--- lib/model/vehc.dart
+++ lib/model/vehc.dart
@@ -1,7 +1,6 @@
 class Vehicle {
   final String id;
   final String name;
-  final String status;
   final String location;
   final int fuelLevel;
   final int batteryLevel;
@@ -10,7 +9,6 @@ class Vehicle {
   Vehicle({
     required this.id,
     required this.name,
-    required this.status,
     required this.location,
     required this.fuelLevel,
     required this.batteryLevel,
@@ -21,7 +19,6 @@ class Vehicle {
     return Vehicle(
       id: json['_id'],
       name: json['name'],
-      status: json['status'],
       location: json['location'] ?? 'Unknown',
       fuelLevel: json['fuelLevel'] ?? 0,
       batteryLevel: json['batteryLevel'] ?? 0,
@@ -32,7 +29,6 @@ class Vehicle {
   Map<String, dynamic> toJson() {
     return {
       'name': name,
-      'status': status,
       'location': location,
       'fuelLevel': fuelLevel,
       'batteryLevel': batteryLevel,
